eb0546e4d0512d074e404ad570e8c522ae822ef1
GastonYasuda <gastonyasuda@gmail.com>
2023-05-16 20:44:58 -0300

    .

--- src/component/ToTop/ToTop.jsx
+++ src/component/ToTop/ToTop.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import upArrow from "../../assets/upArrow.png"
+import pokeNav from "../../assets/pokeNav.png";
+
 
 const ToTop = () => {
     return (
         <button className="upArrow">
             <a href="#top">
                 <img src={upArrow} alt="flecha subir" />
+                <img src={pokeNav} alt="pokeNav" />
+
             </a>
         </button>
     )
