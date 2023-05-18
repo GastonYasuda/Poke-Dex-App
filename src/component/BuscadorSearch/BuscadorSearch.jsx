896d40887966df1780e3f16d25df63a50230c322
GastonYasuda <gastonyasuda@gmail.com>
2023-05-17 23:32:11 -0300

    back depen

--- src/component/BuscadorSearch/BuscadorSearch.jsx
+++ src/component/BuscadorSearch/BuscadorSearch.jsx
@@ -12,7 +12,6 @@ const BuscadorSearch = ({ searchPoke }) => {
 
 
     useEffect(() => {
-        console.log(pokemon);
         setInputCharacter("")
 
         const found = pokemon.find(search => search.name === searchPoke)
